027fafa4ba98c5b183147e671fa17390d9d1514f
gabriel belo <gabrielrbelo@gmail.com>
2020-09-26 14:37:16 -0300

    animal new and create actions created

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -6,9 +6,26 @@ class AnimalsController < ApplicationController
 
   def show; end
 
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      redirect_to animal_path(@animal), notice: 'animal was successfully created'
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_animal
     @animal = Animal.find(params[:id])
+  end
+
+  def animal_params
+    params.require(:animal).permit(:name, :birth_date, :category)
   end
 end
