bdf4763f86c2cbf3ff52005d270f3b3c432b61b6
gabriel belo <gabrielrbelo@gmail.com>
2020-09-26 14:02:56 -0300

    animal show created

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,5 +1,14 @@
 class AnimalsController < ApplicationController
+  before_action :set_animal, only: :show
   def index
     @animals = Animal.all
+  end
+
+  def show; end
+
+  private
+
+  def set_animal
+    @animal = Animal.find(params[:id])
   end
 end
