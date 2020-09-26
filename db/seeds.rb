95611e4034470c165f12512bcc6dbb6bb54e8baf
gabriel belo <gabrielrbelo@gmail.com>
2020-09-26 12:21:45 -0300

    seed created (not tested)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Animal.destroy_all
+
+3.times do
+  animal = Animal.new(name: Faker::Creature::Cat.name,
+                      birth_date: Faker::Date.between(from: '2014-09-23', to: '2019-09-25'),
+                      type: 'cat')
+  animal.save!
+end
+
+3.times do
+  animal = Animal.new(name: Faker::Creature::Dog.name,
+                      birth_date: Faker::Date.between(from: '2014-09-23', to: '2019-09-25'),
+                      type: 'dog')
+  animal.save!
+end
