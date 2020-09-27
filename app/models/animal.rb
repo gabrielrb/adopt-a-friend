70dac4708886693de4ae2111d7d84508e02e30f6
gabriel belo <gabrielrbelo@gmail.com>
2020-09-27 12:55:30 -0300

    friendly_id working with names

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,8 +2,11 @@ class Animal < ApplicationRecord
   has_one :adoption, dependent: :destroy
   has_one_attached :photo
 
+  extend FriendlyId
+  friendly_id :name
+
   validates :name, presence: true,
-                   format: { with: /\A[a-zA-Z]+\z/, message: 'Only letters, without special character' },
+                   format: { with: /\A[a-z]+\z/, message: 'Only downcase letters without special character' },
                    length: { maximum: 100 }
   validates :birth_date, presence: true,
                          format: { with: /\A\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])\z/,
