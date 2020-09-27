92a17f8976fbe9d6cfd8f5633c8df5fb98f31ea9
gabriel belo <gabrielrbelo@gmail.com>
2020-09-27 13:03:05 -0300

    animal name validation updated to accept only downcase names

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -6,7 +6,7 @@ class Animal < ApplicationRecord
   friendly_id :name
 
   validates :name, presence: true,
-                   format: { with: /\A[a-z]+\z/, message: 'Only downcase letters without special character' },
+                   format: { with: /\A[a-z ]+\z/, message: 'Only downcase letters without special character' },
                    length: { maximum: 100 }
   validates :birth_date, presence: true,
                          format: { with: /\A\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])\z/,
