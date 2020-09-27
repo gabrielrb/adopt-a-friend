c1c135e765b8016ca375b1295828b2f903ead73b
gabriel belo <gabrielrbelo@gmail.com>
2020-09-27 12:12:37 -0300

    animal validations updated

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -2,8 +2,13 @@ class Animal < ApplicationRecord
   has_one :adoption, dependent: :destroy
   has_one_attached :photo
 
-  validates :name, presence: true
-  validates :birth_date, presence: true
-  validates :category, presence: true
+  validates :name, presence: true,
+                   format: { with: /\A[a-zA-Z]+\z/, message: 'Only letters, without special character' },
+                   length: { maximum: 100 }
+  validates :birth_date, presence: true,
+                         format: { with: /\A\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])\z/,
+                                   message: 'Needs to follow the format: yyyy-mm-dd' }
+  validates :category, presence: true,
+                       format: { with: /\A(dog|cat|other)\z/, message: 'You need to choose between dog, cat or other.' }
   validates :photo, presence: true
 end
