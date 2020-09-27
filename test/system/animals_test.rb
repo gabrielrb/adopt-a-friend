d028e1039ed802e475f3d7fbe323854f5beffe3f
gabriel belo <gabrielrbelo@gmail.com>
2020-09-27 19:35:25 -0300

    animal index test done

--- test/system/animals_test.rb
+++ test/system/animals_test.rb
@@ -1,10 +1,12 @@
 require 'application_system_test_case'
 
+
 class AnimalsTest < ApplicationSystemTestCase
   test 'visiting the index' do
     visit '/'
 
-    assert_selector 'h2', text: 'Animals'
+    assert_selector 'h2', text: 'Animals available'
+
     assert_selector '.card-product', count: (Animal.count - Adoption.count)
   end
 end
