6be8ba8e47765f335c02438ab7aea22d2a7c20b5
gabriel belo <gabrielrbelo@gmail.com>
2020-09-27 21:04:59 -0300

    adoption create test created

--- test/system/animals_test.rb
+++ test/system/animals_test.rb
@@ -47,7 +47,20 @@ class AnimalsTest < ApplicationSystemTestCase
     # save_and_open_screenshot
   end
 
+  test 'creating a adoption' do
+    visit '/animals/5'
+    # save_and_open_screenshot
 
+    fill_in 'New Owner', with: 'Aline'
+    # save_and_open_screenshot
+
+    click_on 'ADOPT!'
+    # save_and_open_screenshot
+
+    assert_equal animal_path(5), page.current_path
+    assert_text "Animal's Details"
+    # save_and_open_screenshot
+  end
 
   # test 'delete a animal' do
   #   visit '/animals/4'
